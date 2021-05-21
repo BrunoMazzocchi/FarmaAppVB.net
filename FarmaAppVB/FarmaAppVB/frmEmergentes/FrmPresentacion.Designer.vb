1478d4c9d230b91de404c97c5d5d83ebb9f9b7bf
Bruno Mazzocchi <bemazzocchi@gmail.com>
2021-05-20 23:05:38 -0600

    Estilos de frmPresentacion realizados

--- FarmaAppVB/FarmaAppVB/frmEmergentes/FrmPresentacion.Designer.vb
+++ FarmaAppVB/FarmaAppVB/frmEmergentes/FrmPresentacion.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FrmPresentacion
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,14 +20,10 @@ Partial Class FrmPresentacion
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.gbFondo = New System.Windows.Forms.GroupBox()
-        Me.btnNuevo = New System.Windows.Forms.Button()
-        Me.btnEliminar = New System.Windows.Forms.Button()
-        Me.btnEditar = New System.Windows.Forms.Button()
-        Me.btnAgregar = New System.Windows.Forms.Button()
         Me.gbContenedorDGV = New System.Windows.Forms.GroupBox()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.IdPresentacionDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -44,6 +40,10 @@ Partial Class FrmPresentacion
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.PresentacionTableAdapter = New FarmaAppVB.Farma24BDDSTableAdapters.PresentacionTableAdapter()
+        Me.btnEliminar = New MaterialSkin.Controls.MaterialButton()
+        Me.btnEditar = New MaterialSkin.Controls.MaterialButton()
+        Me.btnAgregar = New MaterialSkin.Controls.MaterialButton()
+        Me.btnNuevo = New MaterialSkin.Controls.MaterialButton()
         Me.gbFondo.SuspendLayout()
         Me.gbContenedorDGV.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -55,66 +55,27 @@ Partial Class FrmPresentacion
         'gbFondo
         '
         Me.gbFondo.BackColor = System.Drawing.Color.DodgerBlue
-        Me.gbFondo.Controls.Add(Me.btnNuevo)
         Me.gbFondo.Controls.Add(Me.btnEliminar)
         Me.gbFondo.Controls.Add(Me.btnEditar)
         Me.gbFondo.Controls.Add(Me.btnAgregar)
-        Me.gbFondo.Location = New System.Drawing.Point(576, -9)
+        Me.gbFondo.Controls.Add(Me.btnNuevo)
+        Me.gbFondo.Location = New System.Drawing.Point(768, -11)
+        Me.gbFondo.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.gbFondo.Name = "gbFondo"
-        Me.gbFondo.Size = New System.Drawing.Size(233, 456)
+        Me.gbFondo.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.gbFondo.Size = New System.Drawing.Size(311, 561)
         Me.gbFondo.TabIndex = 0
         Me.gbFondo.TabStop = False
-        '
-        'btnNuevo
-        '
-        Me.btnNuevo.FlatAppearance.BorderColor = System.Drawing.Color.Cyan
-        Me.btnNuevo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.btnNuevo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.btnNuevo.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnNuevo.Location = New System.Drawing.Point(71, 100)
-        Me.btnNuevo.Name = "btnNuevo"
-        Me.btnNuevo.Size = New System.Drawing.Size(87, 30)
-        Me.btnNuevo.TabIndex = 3
-        Me.btnNuevo.Text = "Nuevo"
-        Me.btnNuevo.UseVisualStyleBackColor = True
-        '
-        'btnEliminar
-        '
-        Me.btnEliminar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnEliminar.Location = New System.Drawing.Point(71, 340)
-        Me.btnEliminar.Name = "btnEliminar"
-        Me.btnEliminar.Size = New System.Drawing.Size(87, 30)
-        Me.btnEliminar.TabIndex = 2
-        Me.btnEliminar.Text = "Eliminar"
-        Me.btnEliminar.UseVisualStyleBackColor = True
-        '
-        'btnEditar
-        '
-        Me.btnEditar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnEditar.Location = New System.Drawing.Point(71, 260)
-        Me.btnEditar.Name = "btnEditar"
-        Me.btnEditar.Size = New System.Drawing.Size(87, 30)
-        Me.btnEditar.TabIndex = 1
-        Me.btnEditar.Text = "Editar"
-        Me.btnEditar.UseVisualStyleBackColor = True
-        '
-        'btnAgregar
-        '
-        Me.btnAgregar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnAgregar.Location = New System.Drawing.Point(71, 180)
-        Me.btnAgregar.Name = "btnAgregar"
-        Me.btnAgregar.Size = New System.Drawing.Size(87, 30)
-        Me.btnAgregar.TabIndex = 0
-        Me.btnAgregar.Text = "Agregar"
-        Me.btnAgregar.UseVisualStyleBackColor = True
         '
         'gbContenedorDGV
         '
         Me.gbContenedorDGV.Controls.Add(Me.DataGridView1)
         Me.gbContenedorDGV.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.gbContenedorDGV.Location = New System.Drawing.Point(8, 2)
+        Me.gbContenedorDGV.Location = New System.Drawing.Point(11, 2)
+        Me.gbContenedorDGV.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.gbContenedorDGV.Name = "gbContenedorDGV"
-        Me.gbContenedorDGV.Size = New System.Drawing.Size(562, 247)
+        Me.gbContenedorDGV.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.gbContenedorDGV.Size = New System.Drawing.Size(749, 304)
         Me.gbContenedorDGV.TabIndex = 1
         Me.gbContenedorDGV.TabStop = False
         Me.gbContenedorDGV.Text = "Datos Presentacion"
@@ -128,39 +89,49 @@ Partial Class FrmPresentacion
         Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdPresentacionDataGridViewTextBoxColumn, Me.NombrePresDataGridViewTextBoxColumn, Me.ViaAdminDataGridViewTextBoxColumn, Me.EstadoPresDataGridViewTextBoxColumn})
         Me.DataGridView1.DataSource = Me.PresentacionBindingSource
         Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataGridView1.Location = New System.Drawing.Point(3, 18)
+        Me.DataGridView1.Location = New System.Drawing.Point(4, 23)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.Size = New System.Drawing.Size(556, 226)
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.Size = New System.Drawing.Size(741, 277)
         Me.DataGridView1.TabIndex = 0
         '
         'IdPresentacionDataGridViewTextBoxColumn
         '
         Me.IdPresentacionDataGridViewTextBoxColumn.DataPropertyName = "idPresentacion"
         Me.IdPresentacionDataGridViewTextBoxColumn.HeaderText = "idPresentacion"
+        Me.IdPresentacionDataGridViewTextBoxColumn.MinimumWidth = 6
         Me.IdPresentacionDataGridViewTextBoxColumn.Name = "IdPresentacionDataGridViewTextBoxColumn"
         Me.IdPresentacionDataGridViewTextBoxColumn.ReadOnly = True
+        Me.IdPresentacionDataGridViewTextBoxColumn.Width = 125
         '
         'NombrePresDataGridViewTextBoxColumn
         '
         Me.NombrePresDataGridViewTextBoxColumn.DataPropertyName = "nombrePres"
         Me.NombrePresDataGridViewTextBoxColumn.HeaderText = "nombrePres"
+        Me.NombrePresDataGridViewTextBoxColumn.MinimumWidth = 6
         Me.NombrePresDataGridViewTextBoxColumn.Name = "NombrePresDataGridViewTextBoxColumn"
         Me.NombrePresDataGridViewTextBoxColumn.ReadOnly = True
+        Me.NombrePresDataGridViewTextBoxColumn.Width = 125
         '
         'ViaAdminDataGridViewTextBoxColumn
         '
         Me.ViaAdminDataGridViewTextBoxColumn.DataPropertyName = "viaAdmin"
         Me.ViaAdminDataGridViewTextBoxColumn.HeaderText = "viaAdmin"
+        Me.ViaAdminDataGridViewTextBoxColumn.MinimumWidth = 6
         Me.ViaAdminDataGridViewTextBoxColumn.Name = "ViaAdminDataGridViewTextBoxColumn"
         Me.ViaAdminDataGridViewTextBoxColumn.ReadOnly = True
+        Me.ViaAdminDataGridViewTextBoxColumn.Width = 125
         '
         'EstadoPresDataGridViewTextBoxColumn
         '
         Me.EstadoPresDataGridViewTextBoxColumn.DataPropertyName = "estadoPres"
         Me.EstadoPresDataGridViewTextBoxColumn.HeaderText = "estadoPres"
+        Me.EstadoPresDataGridViewTextBoxColumn.MinimumWidth = 6
         Me.EstadoPresDataGridViewTextBoxColumn.Name = "EstadoPresDataGridViewTextBoxColumn"
         Me.EstadoPresDataGridViewTextBoxColumn.ReadOnly = True
+        Me.EstadoPresDataGridViewTextBoxColumn.Width = 125
         '
         'PresentacionBindingSource
         '
@@ -181,58 +152,66 @@ Partial Class FrmPresentacion
         Me.GroupBox2.Controls.Add(Me.Label3)
         Me.GroupBox2.Controls.Add(Me.Label4)
         Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox2.Location = New System.Drawing.Point(11, 260)
+        Me.GroupBox2.Location = New System.Drawing.Point(15, 320)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(556, 187)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(741, 230)
         Me.GroupBox2.TabIndex = 3
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Ingrese los Datos de la Presentacion"
         '
         'txtIdPres
         '
-        Me.txtIdPres.Location = New System.Drawing.Point(167, 53)
+        Me.txtIdPres.Location = New System.Drawing.Point(223, 65)
+        Me.txtIdPres.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.txtIdPres.Name = "txtIdPres"
-        Me.txtIdPres.Size = New System.Drawing.Size(180, 22)
+        Me.txtIdPres.Size = New System.Drawing.Size(239, 26)
         Me.txtIdPres.TabIndex = 9
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(164, 34)
+        Me.Label5.Location = New System.Drawing.Point(219, 42)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(63, 16)
+        Me.Label5.Size = New System.Drawing.Size(79, 20)
         Me.Label5.TabIndex = 8
         Me.Label5.Text = "ID Pres:"
         '
         'txtViaAdmin
         '
-        Me.txtViaAdmin.Location = New System.Drawing.Point(167, 141)
+        Me.txtViaAdmin.Location = New System.Drawing.Point(223, 174)
+        Me.txtViaAdmin.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.txtViaAdmin.Name = "txtViaAdmin"
-        Me.txtViaAdmin.Size = New System.Drawing.Size(180, 22)
+        Me.txtViaAdmin.Size = New System.Drawing.Size(239, 26)
         Me.txtViaAdmin.TabIndex = 7
         '
         'txtNombreP
         '
-        Me.txtNombreP.Location = New System.Drawing.Point(167, 97)
+        Me.txtNombreP.Location = New System.Drawing.Point(223, 119)
+        Me.txtNombreP.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.txtNombreP.Name = "txtNombreP"
-        Me.txtNombreP.Size = New System.Drawing.Size(180, 22)
+        Me.txtNombreP.Size = New System.Drawing.Size(239, 26)
         Me.txtNombreP.TabIndex = 6
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(164, 122)
+        Me.Label3.Location = New System.Drawing.Point(219, 150)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(162, 16)
+        Me.Label3.Size = New System.Drawing.Size(197, 20)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Via de administracion:"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(164, 78)
+        Me.Label4.Location = New System.Drawing.Point(219, 96)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(67, 16)
+        Me.Label4.Size = New System.Drawing.Size(80, 20)
         Me.Label4.TabIndex = 4
         Me.Label4.Text = "Nombre:"
         '
@@ -240,19 +219,98 @@ Partial Class FrmPresentacion
         '
         Me.PresentacionTableAdapter.ClearBeforeFill = True
         '
+        'btnEliminar
+        '
+        Me.btnEliminar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnEliminar.Depth = 0
+        Me.btnEliminar.DrawShadows = True
+        Me.btnEliminar.HighEmphasis = True
+        Me.btnEliminar.Icon = Nothing
+        Me.btnEliminar.Location = New System.Drawing.Point(96, 366)
+        Me.btnEliminar.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.btnEliminar.MaximumSize = New System.Drawing.Size(158, 36)
+        Me.btnEliminar.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(113, 36)
+        Me.btnEliminar.TabIndex = 7
+        Me.btnEliminar.Text = "       Eliminar      "
+        Me.btnEliminar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.btnEliminar.UseAccentColor = False
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnEditar.Depth = 0
+        Me.btnEditar.DrawShadows = True
+        Me.btnEditar.HighEmphasis = True
+        Me.btnEditar.Icon = Nothing
+        Me.btnEditar.Location = New System.Drawing.Point(96, 299)
+        Me.btnEditar.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.btnEditar.MaximumSize = New System.Drawing.Size(158, 36)
+        Me.btnEditar.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(114, 36)
+        Me.btnEditar.TabIndex = 6
+        Me.btnEditar.Text = "            Editar           "
+        Me.btnEditar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.btnEditar.UseAccentColor = False
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnAgregar
+        '
+        Me.btnAgregar.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnAgregar.Depth = 0
+        Me.btnAgregar.DrawShadows = True
+        Me.btnAgregar.HighEmphasis = True
+        Me.btnAgregar.Icon = Nothing
+        Me.btnAgregar.Location = New System.Drawing.Point(96, 229)
+        Me.btnAgregar.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.btnAgregar.MaximumSize = New System.Drawing.Size(158, 36)
+        Me.btnAgregar.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btnAgregar.Name = "btnAgregar"
+        Me.btnAgregar.Size = New System.Drawing.Size(116, 36)
+        Me.btnAgregar.TabIndex = 5
+        Me.btnAgregar.Text = "        Agregar     "
+        Me.btnAgregar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.btnAgregar.UseAccentColor = False
+        Me.btnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btnNuevo.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btnNuevo.Depth = 0
+        Me.btnNuevo.DrawShadows = True
+        Me.btnNuevo.HighEmphasis = True
+        Me.btnNuevo.Icon = Nothing
+        Me.btnNuevo.Location = New System.Drawing.Point(96, 158)
+        Me.btnNuevo.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.btnNuevo.MaximumSize = New System.Drawing.Size(158, 36)
+        Me.btnNuevo.MouseState = MaterialSkin.MouseState.HOVER
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(119, 36)
+        Me.btnNuevo.TabIndex = 4
+        Me.btnNuevo.Text = "              Nuevo              "
+        Me.btnNuevo.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.btnNuevo.UseAccentColor = False
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
         'FrmPresentacion
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(807, 450)
+        Me.ClientSize = New System.Drawing.Size(1073, 544)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.gbContenedorDGV)
         Me.Controls.Add(Me.gbFondo)
-        Me.MaximumSize = New System.Drawing.Size(823, 489)
-        Me.MinimumSize = New System.Drawing.Size(823, 489)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximumSize = New System.Drawing.Size(1091, 591)
+        Me.MinimumSize = New System.Drawing.Size(1091, 591)
         Me.Name = "FrmPresentacion"
         Me.Text = "Presentacion"
         Me.gbFondo.ResumeLayout(False)
+        Me.gbFondo.PerformLayout()
         Me.gbContenedorDGV.ResumeLayout(False)
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PresentacionBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
@@ -280,8 +338,8 @@ Partial Class FrmPresentacion
     Friend WithEvents txtNombreP As TextBox
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents btnEliminar As Button
-    Friend WithEvents btnEditar As Button
-    Friend WithEvents btnAgregar As Button
-    Friend WithEvents btnNuevo As Button
+    Friend WithEvents btnEliminar As MaterialSkin.Controls.MaterialButton
+    Friend WithEvents btnEditar As MaterialSkin.Controls.MaterialButton
+    Friend WithEvents btnAgregar As MaterialSkin.Controls.MaterialButton
+    Friend WithEvents btnNuevo As MaterialSkin.Controls.MaterialButton
 End Class
